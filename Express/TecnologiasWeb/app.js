1bec14145f2aec505cbbb7ab9bf76f2cb1f52ab9
Andrés David Guerra Vásconez <andreu.g.95@gmail.com>
2016-11-30 14:30:22 -0500

    Manejo de parámetros

--- Express/TecnologiasWeb/app.js
+++ Express/TecnologiasWeb/app.js
@@ -1,6 +1,37 @@
 var express = require('express');
 var app = express();
 
+var usuarios = [
+        {
+            id: 1,
+            nombre: 'Juan',
+            cedula: '1738292839'
+        },
+        {
+            id: 2,
+            nombre: 'Pepe',
+            cedula: '1748392948'
+        },
+        {
+            id: 3,
+            nombre: 'Luis',
+            cedula: '1728392838'
+        }
+    ];
+
+app.get('/Usuarios/:id', function (req, res) {
+   var idActual = req.params.id;
+    for(var i = 0; i < usuarios.length; i++) {
+        if (idActual == usuarios[i].id) {
+            res.json(usuarios[i]);
+            break;
+        }
+        else {
+            res.send('Usuario no encontrado')
+        }
+    }
+});
+
 app.get('/TecnologiasWeb', function (req, res) {
     res.send('Con Get!');
 });
@@ -9,19 +40,22 @@ app.post('/TecnologiasWeb', function (req, res) {
     console.log('Lo que tengo en Request es: ');
     console.log(req.header);
     
-    var usuario = {
+    var parametros = red.params;
+    console.log(parametros);
+    
+    /*var usuario = {
         nombre: 'Andrés',
         cedula: '1723123459'
-    };
+    };*/
     
-    usuario.apellido = '';
+    /*usuario.apellido = '';
     usuario.mascotas = [];
-    usuario.casado = false;
+    usuario.casado = false;*/
     
     res.append('token', '1234');
     
     //res.send('Con Post!');
-    res.json(usuario);
+    //res.json(usuario);
 });
 
 app.put('/TecnologiasWeb', function (req, res) {
