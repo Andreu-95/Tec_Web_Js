c9c0d24be4779797f95cd229815e3cea2dff30b9
Andrés David Guerra Vásconez <andreu.g.95@gmail.com>
2016-11-30 14:55:03 -0500

    Parámetros con Post

--- Express/TecnologiasWeb/app.js
+++ Express/TecnologiasWeb/app.js
@@ -2,38 +2,50 @@ var express = require('express');
 var app = express();
 
 var usuarios = [
-        {
-            id: 1,
-            nombre: 'Juan',
-            cedula: '1738292839'
+    {
+        id: 1,
+        nombre: 'Juan',
+        cedula: '1738292839'
         },
-        {
-            id: 2,
-            nombre: 'Pepe',
-            cedula: '1748392948'
+    {
+        id: 2,
+        nombre: 'Pepe',
+        cedula: '1748392948'
         },
-        {
-            id: 3,
-            nombre: 'Luis',
-            cedula: '1728392838'
+    {
+        id: 3,
+        nombre: 'Luis',
+        cedula: '1728392838'
         }
     ];
 
 app.get('/Usuarios/:id', function (req, res) {
-   var idActual = req.params.id;
+    var idActual = req.params.id;
     for (var i = 0; i < usuarios.length; i++) {
         if (idActual == usuarios[i].id) {
             res.json(usuarios[i]);
             break;
-        }
-        else {
-            res.send('Usuario no encontrado')
+        } else {
+            res.send('Usuario no encontrado');
         }
     }
 });
 
 app.get('/Usuarios', function (req, res) {
-   res.json(usuarios); 
+    console.log(req.param('nombre'));
+    console.log(req.query.nombre);
+
+    res.json(usuarios);
+});
+
+app.post('/Usuarios', function (req, res) {
+    console.log(req.query.nombre);
+    console.log(req.query.cedula);
+    
+    if (!req.query.nombre)
+        res.send('No puso nombre');
+    if (!req.query.cedula)
+        res.send('No puso cédula');
 });
 
 app.get('/TecnologiasWeb', function (req, res) {
@@ -43,21 +55,21 @@ app.get('/TecnologiasWeb', function (req, res) {
 app.post('/TecnologiasWeb', function (req, res) {
     console.log('Lo que tengo en Request es: ');
     console.log(req.header);
-    
-    var parametros = red.params;
+
+    var parametros = req.params;
     console.log(parametros);
-    
+
     /*var usuario = {
         nombre: 'Andrés',
         cedula: '1723123459'
     };*/
-    
+
     /*usuario.apellido = '';
     usuario.mascotas = [];
     usuario.casado = false;*/
-    
+
     res.append('token', '1234');
-    
+
     //res.send('Con Post!');
     //res.json(usuario);
 });
