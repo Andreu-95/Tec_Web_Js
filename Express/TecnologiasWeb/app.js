7dea21e450441706bcf0d74fbc60165d71ede619
Andrés David Guerra Vásconez <andreu.g.95@gmail.com>
2016-11-30 14:09:46 -0500

    Pruebas con JSON

--- Express/TecnologiasWeb/app.js
+++ Express/TecnologiasWeb/app.js
@@ -3,19 +3,25 @@ var app = express();
 
 app.get('/TecnologiasWeb', function (req, res) {
     res.send('Con Get!');
-})
+});
 
-app.post('/TecnologiasWeb', function(req, res) {
+app.post('/TecnologiasWeb', function (req, res) {
     console.log('Lo que tengo en Request es: ');
     console.log(req);
     
-    res.send('Con Post!');
-})
+    var usuario = {
+        nombre: 'Andrés',
+        cedula: '1723123459'
+    };
+    
+    //res.send('Con Post!');
+    res.json(usuario);
+});
 
-app.put('/TecnologiasWeb', function(req, res) {
+app.put('/TecnologiasWeb', function (req, res) {
     res.send('Con Put!');
 })
 
 app.listen(5050, function () {
     console.log('Example app!');
-})
+});
