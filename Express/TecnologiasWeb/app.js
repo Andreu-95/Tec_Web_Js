88ce983b2a4ca50ccc984361f24e71234bb429df
Andrés David Guerra Vásconez <andreu.g.95@gmail.com>
2016-11-30 13:36:43 -0500

    Añadir métodos Post y Put

--- Express/TecnologiasWeb/app.js
+++ Express/TecnologiasWeb/app.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/TecnologiasWeb', function (req, res) {
-    res.send('Hello World!');
+    res.send('Con Get!');
+})
+
+app.post('/TecnologiasWeb', function(req, res) {
+    res.send('Con Post!');
+})
+
+app.put('/TecnologiasWeb', function(req, res) {
+    res.send('Con Put!');
 })
 
 app.listen(5050, function () {
