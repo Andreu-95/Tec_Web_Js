9fd5636d37150571810e3130e8d1828f102e430c
Andrés <andreu.g.95@gmail.com>
2016-12-20 14:57:28 -0500

    Prueba para postman

--- Servidor HTTP/Mascotas/api/controllers/SaludoController.js
+++ Servidor HTTP/Mascotas/api/controllers/SaludoController.js
@@ -7,7 +7,21 @@
 
 module.exports = {
 	hola: function (req, res) {
-        res.send('Hola');
+        if (req.method === 'GET') {
+            res.json({
+                nombre: 'hola get'
+            });
+        }
+        else if (req.method === 'POST') {
+            res.json({
+                nombre: 'hola post'
+            });
+        }
+        else {
+            res.json({
+                nombre: 'hola todos'
+            });
+        }
     },
     
     adios: function (req, res) {
