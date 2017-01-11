2d4d7e4a1194663aa434ff55af3a58024238e863
Andrés <andreu.g.95@gmail.com>
2017-01-10 19:25:02 -0500

    Validar método POST

--- Servidor HTTP/Mascotas/api/controllers/UsuarioController.js
+++ Servidor HTTP/Mascotas/api/controllers/UsuarioController.js
@@ -7,25 +7,29 @@
 
 module.exports = {
     crearUsuario: function (req, res) {
-        
+
         var parametros = req.allParams();
         console.log(parametros);
-        
-        if (parametros.nombres && parametros.apellidos) {
-            Usuario.create({
-            nombres: parametros.nombres,
-            apellidos: parametros.apellidos,
-            correo: parametros.correo
-        }).exec(function (error, usuarioCreado) {
-            if (error) {
-                return res.serverError(error);
-            }
 
-            sails.log.info(usuarioCreado);
-            return res.ok(usuarioCreado);
-        });
+        if (req.method === 'POST') {
+            if (parametros.nombres && parametros.apellidos) {
+                Usuario.create({
+                    nombres: parametros.nombres,
+                    apellidos: parametros.apellidos,
+                    correo: parametros.correo
+                }).exec(function (error, usuarioCreado) {
+                    if (error) {
+                        return res.serverError(error);
+                    }
+
+                    sails.log.info(usuarioCreado);
+                    return res.ok(usuarioCreado);
+                });
+            } else {
+                return res.badRequest('No envía todos los parámetros');
+            }
         } else {
-            return res.badRequest('No envía todos los parámetros');
+            return res.badRequest('Método inválido');
         }
     }
 };
