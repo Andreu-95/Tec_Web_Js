258695e346a28198f51ac1dc57ff625c542930f4
Andrés Guerra <andreu.g.95@gmail.com>
2017-02-23 18:02:04 -0500

    Pruebas Angular

--- angPrueba/src/app/app.component.ts
+++ angPrueba/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  title: string = 'Hola todos';
+  nombre: string = "";
+  apellido: string = "";
+  font = '25px';
+  classes = 'btn btn-success btn-block';
+
+  nuevaTienda: any = {};
+
+  constructor() {
+    this.apellido = 'Guerra';
+    this.nombre = 'Andres';
+    console.log('Inicio el constructor');
+  }
+
+  ngOnInit() {
+    this.apellido = 'Vásconez';
+    this.nombre = 'David';
+    console.log('On Init')
+  }
+
+  nombreCompleto(): string {
+    return `${this.nombre} ${this.apellido}`
+  }
+
+  hizoClic() {
+    console.log('Hizo clic')
+  }
+
+  hizoFocus() {
+    console.log('Hizo focus')
+  }
 }
